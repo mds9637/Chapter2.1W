a658d2c86e30671c89051a47073808ee3520e89f
Matthew Sharps <mds9637@email.vccs.edu>
2025-01-31 11:47:23 -0500

    update

--- Chapter2.1W.cpp
+++ Chapter2.1W.cpp
@@ -1,11 +1,32 @@
-// Chapter2.1W.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+File Name: Chapter2.1W.cpp
+Developer: Matthew Sharps
+Date: January 31, 2025
+
+Requirements:
+Score 20 in speed variable
+Score 10 in time variable
+Multiply speed by time
+Display contents of distance variable
+*/
+
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double force;
+	double area;
+	double pressure;
+
+	cout << "Enter a force value ";
+	cin >> force;
+	cout << "Enter the area value ";
+	cin >> area;
+
+	pressure = force / area;
+	cout << "The pressure of the the area with the force provided is " << pressure;
 }
 
 
